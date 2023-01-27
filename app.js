5c48681729fb8043bd9d6bbb36b29f923a119cc2
Gabriela Custódio <gabrielapcstd@gmail.com>
2023-01-27 00:55:01 +0000

    feat(app.js): create variables

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 
 // import {} from './lib/code.org.js';
 
+let economia = prompt("Quanto você quer economizar?");
+let carteira = 200;
+const comida = 30;
+const brinquedo = 15;
+const circo = 20;
+
+
+
+function fazCompra() {
+
+};
