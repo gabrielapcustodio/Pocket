5e58928dea0accdae857abefeb6cb70f03d1fc5c
gabrielapcustodio <82176005+gabrielapcustodio@users.noreply.github.com>
2023-01-28 18:13:55 -0300

    new functions

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-// definir valores das variáveis
+import { cadastraInformacoes, realizaCompra, alertaPoupanca, criaExtrato, atualizaPagina } from './lib/newFunctions.js';
+
+// definir variáveis
 
 const infoBotao = document.getElementById("infoButton");
 const refreshBotao = document.getElementById("refresh-page");
-const botaoComida = document.getElementById("comida");
-const botaoBrinquedo = document.getElementById("brinquedo");
-const botaoCirco = document.getElementById("circo");
+const botaoExtrato = document.getElementById("go-extrato");
+
+const tituloComida = document.getElementById("titleComida").innerHTML;
+const tituloBrinquedo = document.getElementById("titleBrinquedo").innerHTML;
+const tituloCirco = document.getElementById("titleCirco").innerHTML;
 
 let valorCarteira = document.getElementById("valorCarteira");
 let orcamento = document.getElementById("orcamento");
@@ -14,68 +18,22 @@ const valorComida = 50;
 const valorBrinquedo = 35;
 const valorCirco = 40;
 
-// usuário preenche orçamento e economia  e o código
-//pega o valor de orçamento e insere na carteira
 
-infoBotao.addEventListener("click", function (event) {
-  event.preventDefault();
-  orcamento = orcamento.value;
-  economia = economia.value;
-  infoBotao.disabled = true;
-  atualizaValor();
-});
+// Início da operação
+cadastraInformacoes(infoBotao, orcamento, economia, valorCarteira);
 
-// checa se a carteira está vazia
+let extratoDosGastos = [];
 
-function checaVazio(idVariavel) {
-  return document.getElementById(idVariavel).innerHTML === "";
-}
+// Compras
+realizaCompra("comida", valorCarteira, economia, valorComida, tituloComida, "spanValue");
 
-// atualiza o valor da carteira
+realizaCompra("brinquedo", valorCarteira, economia, valorBrinquedo, tituloBrinquedo, "spanValue");
 
-function atualizaValor() {
-    valorCarteira.innerHTML = orcamento;
-}
+realizaCompra("circo", valorCarteira, economia, valorCirco, tituloCirco, "spanValue");
 
-// autoriza preenchimento se o formulário estiver preenchido
+// Extrato
 
-function permissaoParaPreencher(tipoGasto) {
-  if (checaVazio("valorCarteira") == false) {
-    orcamento -= tipoGasto;
-    atualizaValor();
-  }
-}
+criaExtrato(botaoExtrato, extratoDosGastos);
 
-// O valor que deve ser guardado é utilizado para checar se o que ainda tem na carteira é maior ou igual ao que se quer economizar
-
-function alertaPoupanca() {
-    if (orcamento <= economia) {
-        valorCarteira.style.color = 'red';
-    }
-}
-
-
-// ao clicar em cada botão de despesa, o valor é reduzido da carteira
-
-botaoComida.addEventListener("click", function () {
-  permissaoParaPreencher(valorComida);
-  alertaPoupanca();
-});
-
-botaoBrinquedo.addEventListener("click", function () {
-  permissaoParaPreencher(valorBrinquedo);
-  alertaPoupanca();
-});
-
-botaoCirco.addEventListener("click", function () {
-  permissaoParaPreencher(valorCirco);
-  alertaPoupanca();
-});
-
-// Ao final, o usuário pode atualizar a página
-
-refreshBotao.addEventListener("click", function (event) {
-    event.preventDefault();
-    window.location.reload();
-});
-
+// Finaliza a operação
+atualizaPagina(refreshBotao);
